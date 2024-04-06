c950fdb3ec9b132b50b9c98e6ddbba59fdcce087
habini86 <vinicius1234585@gmail.com>
2024-04-06 17:35:50 -0300

    remove: removendo codigo

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
-    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log(`HTTP Server Running! Port: ${env.PORT}`)
